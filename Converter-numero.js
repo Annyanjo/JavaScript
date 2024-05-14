70743231c2a79137c41b397646186c67505c6719
Annyanjo <anelliz34dosanjosmelloesilva@gmail.com>
2024-05-14 17:50:34 -0400

    Converter-numero.js

---
+++ Converter-numero.js
@@ -0,0 +1,7 @@
+let concatencao = "1.1" + "1.1";
+let converterParaNumero = +"1.1";
+console.log(concatencao); // "1.11.1"
+console.log(typeof(concatencao)); // string
+console.log("");
+console.log(converterParaNumero); // 1.1
+console.log(typeof(converterParaNumero)); // number
